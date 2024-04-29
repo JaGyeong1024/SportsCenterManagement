31030dc494f3b24c1a74a6083665b59356969fb9
J.K.Koo <92356313+JaGyeong1024@users.noreply.github.com>
2024-04-30 04:06:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include <string.h>
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable:4996)
-#define FILENAME "sportscenter.csv"
-#define NowMember 10
-#define MAXLEN 50
-#define NewMemberNumber 0
-int increased = 1;
-
-typedef struct s_memberinfo {
-	int MemberNumber;
-	char name[MAXLEN]; //동적할당으로 바꾸기
-	int gender; //3이면 남자 4이면 여자
-	char PhoneNumber[12];
-	int state; // 0이면 퇴실 1이면 입실
-}MemberInfo;
 int main() {
 
 	MemberInfo* RTUser;
@@ -25,7 +6,9 @@ int main() {
 	int num = 0;
 
 	while (1) {
+		printf("===============================================================================================\n");
 		printf("0.현재이용인원 1.회원입장 2.회원퇴장 3.회원등록 4.회원삭제 5.회원정보검색 9.프로그램 종료\n");
+		printf("===============================================================================================\n");
 		scanf("%d", &num);
 		getchar();
 
@@ -39,26 +22,33 @@ int main() {
 		else {
 			int MemberNumber;
 			switch (num) {
-			/*case 1:
-				printf("회원번호를 입력해주십시오: ");
-				scnaf("%d", &MemberNumber);
-				MemberEnter(MemberNumber, RTUser);
-				break;
-			case 2:
-				MemberExit(MemberNumber);
-				break;*/
+				/*case 1:
+					printf("회원번호를 입력해주십시오: ");
+					scnaf("%d", &MemberNumber);
+					MemberEnter(MemberNumber, RTUser);
+					break;
+				case 2:
+					MemberExit(MemberNumber);
+					break;*/
 			case 3:
 				MemberRegi();
 				break;
-			/*case 4:
-				MemberDel(MemberNumber);
-				break;
+				/*case 4:
+					MemberDel(MemberNumber);
+					break;
+					*/
 			case 5:
-				MemberSearch(MemberNumber);
-				break;*/
+
+				int SMemberNumber;
+
+				printf("회원번호를 입력해주세요: ");
+				scanf("%d", &SMemberNumber);
+				getchar();
+
+				MemberSearch(SMemberNumber);
+
 			}
 		}
-
 	}
 
 	return 0;
