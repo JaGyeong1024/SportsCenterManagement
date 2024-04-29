a04f725b21cce9b7c10fa90cf44fc328c70896f0
J.K.Koo <92356313+JaGyeong1024@users.noreply.github.com>
2024-04-30 02:38:10 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,73 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<stdbool.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#define FILENAME "sportscenter.csv"
+#define NowMember 10
+#define MAXLEN 50
+#define NewMemberNumber 0
+int increased = 1;
 
-struct memberinfo {
+typedef struct s_memberinfo {
 	int MemberNumber;
-	char name[9]; //동적할당으로 바꾸기
-	char sex;
+	char name[MAXLEN]; //동적할당으로 바꾸기
+	int gender; //3이면 남자 4이면 여자
 	char PhoneNumber[12];
-	bool state;
-};
-void RealTimeUser() {
-	//현재 이용인원
-}
-void MemberEnter(int MemberNumber) {
-	//회원 입장
-}
-void MemberExit(int MemberNumber) {
-	//회원 퇴장
-}
-void MemberRegi() {
-	//회원 등록
-}
-void MemberDel(int MemberNumber) {
-	//회원 삭제
-}
-void MemberSearch(int MemberNumber) {
-	//회원정보검색
-	//유효하지 않은 회원번호일 시, 유효하지 않은 회원번호입니다. 띄우기
-}
+	int state; // 0이면 퇴실 1이면 입실
+}MemberInfo;
 int main() {
+
+	MemberInfo* RTUser;
+	RTUser = (MemberInfo*)malloc(sizeof(MemberInfo) * NowMember);
+
 	int num = 0;
+
 	while (1) {
-		printf("0.현재이용인원 1.회원입장 2.회원퇴장 3.회원등록 4.회원삭제 5.회원정보검색 9.프로그램 종료");
-		scanf_s("%d", &num);
+		printf("0.현재이용인원 1.회원입장 2.회원퇴장 3.회원등록 4.회원삭제 5.회원정보검색 9.프로그램 종료\n");
+		scanf("%d", &num);
+		getchar();
+
 		if (num == 9) {
+			free(RTUser);
 			break;
 		}
-		else if(num == 0) {
-			RealTimeUser();
-		} 
-		else{
+		else if (num == 0) {
+			//RealTimeUserShow();
+		}
+		else {
 			int MemberNumber;
 			switch (num) {
-			case 1:
+			/*case 1:
 				printf("회원번호를 입력해주십시오: ");
-				scnaf_s("%d", &MemberNumber);
-				MemberEnter(MemberNumber);
+				scnaf("%d", &MemberNumber);
+				MemberEnter(MemberNumber, RTUser);
 				break;
 			case 2:
 				MemberExit(MemberNumber);
-				break;
+				break;*/
 			case 3:
 				MemberRegi();
 				break;
-			case 4:
+			/*case 4:
 				MemberDel(MemberNumber);
 				break;
 			case 5:
 				MemberSearch(MemberNumber);
-				break;
+				break;*/
 			}
 		}
 
-
-
-
-
-
 	}
+
 	return 0;
 }
