0b16e222dba972e10d7786f3bd30d858542ce04c
J.K.Koo <92356313+JaGyeong1024@users.noreply.github.com>
2024-05-01 07:27:49 +0900

    Create CurrentMemberShow.c

---
+++ CurrentMemberShow.c
@@ -0,0 +1,36 @@
+int CurrentMemberShow() {
+	MemberInfo* members;
+	int numMembers;
+	int count = 0;
+
+	FILE* file = fopen(FILENAME, "r");
+	if (file == NULL) {
+		printf("파일을 열 수 없습니다.\n");
+		exit(1);
+	}
+
+	numMembers = 0;
+	members = NULL;
+	char line[256];
+
+	while (fgets(line, sizeof(line), file)) {
+		members = realloc(members, (numMembers + 1) * sizeof(MemberInfo));
+		MemberInfo* currentMember = &(members)[numMembers];
+
+		sscanf(line, "%d, %[^,], %d, %[^,], %d", &currentMember->MemberNumber, currentMember->name, &currentMember->gender, currentMember->PhoneNumber, &currentMember->state);
+
+		(numMembers)++;
+	}
+
+	fclose(file);
+
+	for (int i = 0; i < numMembers; i++) {
+		if (members[i].state == 1) {
+			count++;
+		}
+	}
+
+	free(members);
+
+	return count;
+}
