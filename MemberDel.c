c80ca0d0feea0a2d57db34dd6046ffe017876397
J.K.Koo <92356313+JaGyeong1024@users.noreply.github.com>
2024-05-01 23:25:18 +0900

    Update MemberDel.c

--- MemberDel.c
+++ MemberDel.c
@@ -48,34 +48,3 @@ void MemberDel(int SMemberNumber) {
 
 	printf("회원 번호 %d에 해당하는 회원을 삭제했습니다.\n", SMemberNumber);
 }
-void MemberSearch(int SMemberNumber) {
-	// CSV 파일에서 조건에 맞는 행을 가져오는 함수
-	printf("-----------------------------------------------------------------------------------------------\n");
-	FILE* file = fopen(FILENAME, "r");
-	if (file == NULL) {
-		printf("파일을 열 수 없습니다.\n");
-		return;
-	}
-
-	char line[256];
-	while (fgets(line, sizeof(line), file)) {
-		int MemberNumber = 0;
-		char name[MAXLEN];
-		int gender = 0;
-		char PhoneNumber[12];
-		int state = 0;
-
-		sscanf(line, "%d, %[^,], %d, %[^,], %d", &MemberNumber, name, &gender, PhoneNumber, &state);
-
-		if (MemberNumber == SMemberNumber) {
-			printf("회원번호, 이름, 성별, 전화번호, (0:퇴실, 1:입실)\n");
-			printf(" %d,  %s, %d,  %s,      %d\n", MemberNumber, name, gender, PhoneNumber, state);
-			fclose(file);
-			return;
-		}
-		else {
-			printf("유효하지 않은 회원번호입니다.\n");
-			return;
-		}
-	}
-}
